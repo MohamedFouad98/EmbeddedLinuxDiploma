c333b4f4e636e6dce6c26a06ddad62b4c9b08f7d
unknown <fo2sh@DESKTOP-VLFRJ3N.unepp.local>
2023-08-11 03:40:43 +0300

    add Session1_Session2

---
+++ Session1_Session2/Task3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# import os module
+import os
+  
+# display all environment variable
+print('----------------------------------------------------------------')
+print(os.environ['PATH'])
+print('----------------------------------------------------------------')
+print(os.environ['OS'])
+print('----------------------------------------------------------------')
---
+++ Session1_Session2/Task7.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import requests
+list=[50,70,200,300,30,60]
+
+print(max(list))
