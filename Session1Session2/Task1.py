7ced753c5100063ecac938a9b821ae72812dc65f
unknown <fo2sh@DESKTOP-VLFRJ3N.unepp.local>
2023-08-12 20:02:28 +0300

    add Session1Session2

--- Session1Session2/Task1.py
+++ Session1Session2/Task1.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def list_count_4(nums):
-  count = 0  
-  for num in nums:
-    if num == 4:
-      count +=1
-
-  return count
-
-print(list_count_4([1, 4, 4, 4, 4]))
-print(list_count_4([1, 2, 2, 4, 4, 4]))
+List = (input("please enter list:"))
+val_toSearch= (input("please enter the number you are searching for:"))
+print("This value repeated in the number "+str(List.count(val_toSearch))+" times")
