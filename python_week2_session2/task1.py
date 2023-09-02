36d28adda59e9a9e9a1434aa5e83c53c91ea7d8f
unknown <fo2sh@DESKTOP-VLFRJ3N.unepp.local>
2023-09-02 15:26:36 +0300

    fix

--- python_week2_session2/task1.py
+++ python_week2_session2/task1.py
@@ -1,7 +1,7 @@
 def generate_gpio_init_function(ddra_value):
     ddra_binary = ''.join(map(str, ddra_value))
     init_function = f"""
-void gpio_init() {{
+void gpio_init(){{
     // Initialize GPIO pins here
     DDRA = 0b{ddra_binary};  // Set DDRA value
 }}
@@ -15,8 +15,9 @@ def main():
 
     for pin in range(pin_count):
         value = int(input(f"Enter value (0 or 1) for Pin {pin}: "))
+        value = 1 if value > 1 else value
         ddra_value.append(value)
-
+    ddra_value.reverse()
     init_code = generate_gpio_init_function(ddra_value)
 
     with open("gpio_init.c", "w") as file:
